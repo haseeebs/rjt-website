8ed13eb0fca866557c5493cf37cba3fbeb6f9484
haseeb hummo <haseebpersonalwork@gmail.com>
2024-09-26 19:09:51 +0530

    Update configuration file

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,13 @@ const config = {
     databaseId: String(import.meta.env.VITE_APPWRITE_DATEBASE_ID),
     packageCollectionId: String(import.meta.env.VITE_APPWRITE_PACKAGE_COLLECTION),
     hotelCollectionId: String(import.meta.env.VITE_APPWRITE_HOTEL_COLLECTION),
-    bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    commonInclusionsCollectionId: String(import.meta.env.VITE_APPWRITE_COMMON_INCLUSIONS_COLLECTION),
+    foodImagesCollectionId: String(import.meta.env.VITE_APPWRITE_FOOD_IMAGES_COLLECTION),
+    foodImagesBucketId: String(import.meta.env.VITE_APPWRITE_FOOD_IMAGES_BUCKET_ID),
+    bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    customizePackageRequestCollectionId: String(import.meta.env.VITE_APPWRITE_CUSTOMIZE_PACKAGE_REQUEST_COLLECTION_ID),
+    notificationsCollectionId: String(import.meta.env.VITE_APPWRITE_NOTIFICATIONS_COLLECTION_ID),
+    instagramAccessToken: String(import.meta.env.VITE_INSTAGRAM_ACCESS_TOKEN),
 };
 
 export default config;
